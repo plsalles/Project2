9f9e55df4d869592265fb790fbc803dbf0572867
Joao Juliatti <joaopjuliatti@gmail.com>
2020-04-14 21:01:19 -0300

    new routes for patient and doctor

--- routes/private/paciente.routes.js
+++ routes/private/paciente.routes.js
@@ -7,8 +7,54 @@ const passport = require('passport');
 const flash = require('connect-flash');
 
 router.get('/intranet', (req, res, next) => {
-  //console.log(req.user);
   res.render('private/intranet', { message: req.flash('error') });
 });
+
+
+router.get('/home', (req, res, next) => {
+  res.render('private/intranet', { message: req.flash('error') });
+});
+
+
+router.get('/consultas', (req, res, next) => {
+  const { role } = req.user
+  if(role==='PACIENTE'){
+    res.render('private/paciente/consultas', { message: req.flash('error') });
+  } 
+  else if(role ==='MEDICO'){
+    res.render('private/medico/consultas', { message: req.flash('error') });
+  }
+  else {
+    res.render('public/login', { message: req.flash('error') });
+  }
+  
+});
+
+router.get('/agenda', (req, res, next) => {
+  const { role } = req.user
+  if(role==='PACIENTE'){
+    res.render('private/paciente/agenda', { message: req.flash('error') });
+  } 
+  else if(role ==='MEDICO'){
+    res.render('private/medico/agenda', { message: req.flash('error') });
+  }
+  else {
+  }  res.render('public/login', { message: req.flash('error') });
+  
+});
+
+router.get('/dados', (req, res, next) => {
+  const { role } = req.user
+  if(role==='PACIENTE'){
+    res.render('private/paciente/dados', { message: req.flash('error') });
+  } 
+  else if(role ==='MEDICO'){
+    res.render('private/medico/dados', { message: req.flash('error') });
+  }
+  else {
+  }  res.render('public/login', { message: req.flash('error') });
+  
+});
+
 
 module.exports = router;
