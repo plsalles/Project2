42def90a69ddf4f31a8e5918930f6a4acc4b4451
plsalles <plsalles@gmail.com>
2020-04-22 22:20:03 -0300

    resolvendo ultimos conflitos

--- models/Medico.js
+++ models/Medico.js
@@ -9,11 +9,7 @@ const medicoSchema = new Schema({
   email: { type: String, required: true, unique: true },
   CRM: { type: String, require: true, unique: true},
   especializacao: { type: String, require:true},
-<<<<<<< HEAD
-  endereco: { logradouro: { type: String, require: true}, numero: {type: Number, require: true}, complemento: {type: String}, bairro: { type: String, require: true}, cidade: { type: String, require: true}, estado: { type: String, require: true}, cep: {type: String, require: true},pais: { type: String, require: true}},
-=======
-  endereco: { logradouro: { type: String, require: true}, numero: {type: Number, require: true}, bairro: { type: String, require: true}, cidade: { type: String, require: true}, estado: { type: String, require: true}, cep: {type: String, require: true},},
->>>>>>> c07c360008dd38a53ef480e31c1f3b4fc69aab43
+  endereco: { logradouro: { type: String, require: true}, numero: {type: Number, require: true}, complemento: {type: String}, bairro: { type: String, require: true}, cidade: { type: String, require: true}, estado: { type: String, require: true}, cep: {type: String, require: true}},
   },
   {
     timestamps:true
--- models/Paciente.js
+++ models/Paciente.js
@@ -7,11 +7,7 @@ const pacienteSchema = new Schema({
   email: { type: String, required: true, unique: true },
   name: { type: String, require:true},
   cpf: { type: String, require: true, unique: true},
-<<<<<<< HEAD
-  endereco: { logradouro: { type: String, require: true}, numero: {type: Number, require: true}, bairro: { type: String, require: true}, complemento: { type: String}, cidade: { type: String, require: true}, estado: { type: String, require: true}, cep: {type: String, require: true}, pais: { type: String, require: true}},
-=======
-  endereco: { logradouro: { type: String, require: true}, numero: {type: Number, require: true}, bairro: { type: String, require: true}, cidade: { type: String, require: true}, estado: { type: String, require: true}, cep: {type: String, require: true},},
->>>>>>> c07c360008dd38a53ef480e31c1f3b4fc69aab43
+  endereco: { logradouro: { type: String, require: true}, numero: {type: Number, require: true}, bairro: { type: String, require: true}, complemento: { type: String}, cidade: { type: String, require: true}, estado: { type: String, require: true}, cep: {type: String, require: true}},
   medicos: [{ type: mongoose.Types.ObjectId, ref: 'Medico'}],
   user: { type: mongoose.Types.ObjectId, ref: 'User'},
   },
