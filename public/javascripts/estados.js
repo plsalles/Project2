b6feb56762662aecebd00de2b5c210a8400e10aa
plsalles <plsalles@gmail.com>
2020-04-30 20:42:57 -0300

    Resolvendo problema de popular estado correto

---
+++ public/javascripts/estados.js
@@ -0,0 +1,21 @@
+const element = document.querySelector('#dados-user');
+console.log(element);
+const userId = element.getAttribute('user');
+const role = element.getAttribute('role');
+
+axios.get(`http://localhost:3000/api/${role}/${userId}`)
+.then(data => {
+  const user = data.data;
+  const states = ["AC","AL","AP","AM","BA","CE","DF","ES","GO","MA","MT","MS","MG","PA","PB","PR","PE","PI","RJ","RN","RS","RO","RR","SC","SP","SE","TO"]
+  let estados = '';
+  for(let i=0;i<states.length;i++){
+    if(states[i]===user.endereco.estado){
+      estados = estados+`<option value="${states[i]}" selected="selected">${states[i]}</option>`
+    }else  estados = estados+`<option value="${states[i]}">${states[i]}</option>`
+}
+
+  document.getElementById('estados').innerHTML = estados;
+ 
+
+})
+.catch(error => console.log(error));
