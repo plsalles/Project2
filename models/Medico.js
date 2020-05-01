c1106fb52a67f7687ee53aa67e511d6eef73de1d
Joao Paulo Juliatti <joaopjuliatti@gmail.com>
2020-04-30 21:42:28 -0300

    fix : signup users (#30)
    * sign-up adjust to no accept already used username and crm or cpf
    
    * urls to heroku url

--- models/Medico.js
+++ models/Medico.js
@@ -6,7 +6,7 @@ const { Schema } = mongoose;
 const medicoSchema = new Schema({
   user: { type: mongoose.Types.ObjectId, ref: 'User', unique:true},
   name: { type: String, require:true},
-  email: { type: String, required: true, unique: true },
+  email: { type: String, required: true},
   CRM: { type: String, require: true, unique: true},
   especializacao: { type: String, require:true},
   endereco: { logradouro: { type: String, require: true}, numero: {type: Number, require: true}, complemento: {type: String}, bairro: { type: String, require: true}, cidade: { type: String, require: true}, estado: { type: String, require: true}, cep: {type: String, require: true}},
